3ee9169965f22f4fba1316894eb5dfc2977ab7c8
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-09-30 17:24:56 +0300

    Updated hex orientation

--- HexWar/Assets/Scripts/HexMetrics.cs
+++ HexWar/Assets/Scripts/HexMetrics.cs
@@ -6,12 +6,28 @@ public static class HexMetrics
 
 	public const float innerRadius = outerRadius * 0.866025404f;
 
-	public static Vector3[] corners = {
-		new Vector3(0f, 0f, outerRadius),
-		new Vector3(innerRadius, 0f, 0.5f * outerRadius),
-		new Vector3(innerRadius, 0f, -0.5f * outerRadius),
-		new Vector3(0f, 0f, -outerRadius),
-		new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
-		new Vector3(-innerRadius, 0f, 0.5f * outerRadius)
+	/// <summary>
+	/// Top orientation
+	/// </summary>
+	//public static Vector3[] corners = {
+	//	new Vector3(0f, 0f, outerRadius),
+	//	new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+	//	new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+	//	new Vector3(0f, 0f, -outerRadius),
+	//	new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+	//	new Vector3(-innerRadius, 0f, 0.5f * outerRadius)
+	//};
+
+	/// <summary>
+	/// Left orientation
+	/// </summary>
+	public static Vector3[] corners =
+	{
+		new Vector3(-outerRadius, 0f, 0f),
+		new Vector3(-0.5f * outerRadius, 0f, innerRadius),
+		new Vector3(0.5f * outerRadius, 0f, innerRadius),
+		new Vector3(outerRadius, 0f, 0f),
+		new Vector3(0.5f * outerRadius, 0f, -innerRadius),
+		new Vector3(-0.5f * outerRadius, 0f, -innerRadius)
 	};
 }
